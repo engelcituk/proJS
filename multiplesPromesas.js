186f9c4cd5452587c55cc81f1f75e2f9c60f01ac
citkcaamal <programaciondyn3.mx@sandos.com>
2019-11-26 11:01:16 -0500

    promesas y resolver multiples promesas

---
+++ multiplesPromesas.js
@@ -0,0 +1,16 @@
+// para resolver multiples promesas
+let promesa1 = new Promise( (resolve, reject) => setTimeout( resolve, 500, 'hola mundo'));
+
+let promesa2 = new Promise( (resolve, reject) => setTimeout( resolve, 600, 'segundo hola mundo'));
+
+let promesa3 = Promise.reject();
+
+
+let saluda = () =>  console.log('Hola a todos ...');
+
+// promesa1.then(console.log);
+
+Promise.all([promesa1, promesa2, promesa3]).then( resultados => {
+    console.log(resultados);
+    saluda();
+}).catch( () => console.log('he fallado'));
